c353ae906620548c480a2035df6bba06f3a30d6f
nicoviarnes <nlviarnes@gmail.com>
2019-05-09 23:48:45 -0700

    trying to fix client folder

--- routes/api/test.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../../models/Users");
-
-router.get("/", (req, res) => {
-	User.find().then(users => res.json(users));
-});
-
-module.exports = router;
