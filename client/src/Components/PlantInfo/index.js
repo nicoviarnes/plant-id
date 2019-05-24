b96b2656fdc4f726ec34af6e82e8859208320074
Lap Larry Nguyen <larrylapnguyen@gmail.com>
2019-05-23 18:32:14 -0700

    note api

--- client/src/Components/PlantInfo/index.js
+++ client/src/Components/PlantInfo/index.js
@@ -7,7 +7,8 @@ import NoteForm from "../NoteForm";
 class PlantInfo extends React.Component {
 	state = {
 		info: null,
-		plantId: ""
+		plantId: "",
+		notes: null
 	};
 
 	componentWillMount() {
@@ -19,6 +20,18 @@ class PlantInfo extends React.Component {
 			this.setState({ plantId: plantID });
 			this.setState({ info: res.data.filter(dat => dat._id === plantID) });
 		});
+		console.log(this.state.Info);
+	}
+	componentDidMount() {
+		const plantID = this.props.match.params.plant;
+		console.log(plantID);
+		API.getPlantNote(plantID).then(res => {
+			this.setState({
+				notes: res.data.filter(data => data.plant === plantID)
+			});
+			console.log(this.state.notes);
+		});
+		// .then(console.log(this.state.notes));
 	}
 
 	render() {
@@ -39,12 +52,19 @@ class PlantInfo extends React.Component {
 						<Grid item xs={12} sm={6}>
 							<NoteForm plantID={this.state.plantId} />
 						</Grid>
+
 						<Grid item xs={12} sm={12}>
 							<div className="my-notes-wrap">
 								<h2>My Notes</h2>
-								<div className="notes-target" />
+								<div className="notes-target">
+									{this.state.notes &&
+										Object.entries(this.state.notes[0]).map((note, i) => {
+											return <li key={i}>{note[1]}</li>;
+										})}
+								</div>
 							</div>
 						</Grid>
+
 						<Grid item xs={12} sm={12}>
 							{this.state.info[0].plantInfo &&
 								Object.entries(this.state.info[0].plantInfo[0]).map(
