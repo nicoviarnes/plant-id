d002b0b7fbfbadac174939c1c4e02e879a8f60f5
nicoviarnes <nlviarnes@gmail.com>
2019-05-28 21:12:28 -0700

    change token expiration

--- routes/api/users.js
+++ routes/api/users.js
@@ -43,7 +43,7 @@ router.post("/", (req, res) => {
 						//get jwtsecret from config folder
 						config.get("jwtSecret"),
 						//Set expire to 1 hour
-						{ expiresIn: "20d" },
+						{ expiresIn: "1d" },
 						//Call Back Async
 						(err, token) => {
 							if (err) throw err;
