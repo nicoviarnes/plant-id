014bd8938c68a45820ed863119a43b29f12bbbb4
Lap Larry Nguyen <larrylapnguyen@gmail.com>
2019-05-28 19:45:04 -0700

    font, condit (mobile)

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -105,8 +105,8 @@ function ButtonAppBar(props) {
 									{/* Home Button */}
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										width="24"
-										height="24"
+										width="30"
+										height="30"
 										viewBox="0 0 24 24"
 									>
 										<path fill="none" d="M0 0h24v24H0V0z" />
