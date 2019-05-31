d3f16e8baa985d6f55b972133a9bd67469f29d7d
nicoviarnes <nlviarnes@gmail.com>
2019-05-30 18:16:58 -0700

    build for heroku

---
+++ client/build/precache-manifest.89ac44adc7b76da921a39b5ca5af18fa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc4f542b7e460cc323dcea2fbf0b20d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2a32cd6e8361bca156",
+    "url": "/static/css/2.c49477c7.chunk.css"
+  },
+  {
+    "revision": "38a66c0a855a4ebd61d7",
+    "url": "/static/css/main.13e6e317.chunk.css"
+  },
+  {
+    "revision": "ca2a32cd6e8361bca156",
+    "url": "/static/js/2.69e66fed.chunk.js"
+  },
+  {
+    "revision": "38a66c0a855a4ebd61d7",
+    "url": "/static/js/main.d097fba9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "301803109a4f24bf0fd4210cb10e0e44",
+    "url": "/static/media/aloe.30180310.jpg"
+  },
+  {
+    "revision": "b855410fc766e7fe8732822cdcdbfa02",
+    "url": "/static/media/badExample.b855410f.jpg"
+  },
+  {
+    "revision": "12bc5aaeca335a08c1d566b842eb5fbf",
+    "url": "/static/media/goodExample.12bc5aae.jpg"
+  },
+  {
+    "revision": "5150ee4331803f63c7fe23535a10c58d",
+    "url": "/static/media/hands.5150ee43.jpg"
+  },
+  {
+    "revision": "af35a29de955bf71171ac693a0273a92",
+    "url": "/static/media/idMobile.af35a29d.jpeg"
+  },
+  {
+    "revision": "5432c33be55b8872aa9ef4a50ecf61fb",
+    "url": "/static/media/ivy.5432c33b.jpg"
+  },
+  {
+    "revision": "2d28f493eaf5f961c214a1cb855ef8c6",
+    "url": "/static/media/larryimg.2d28f493.jpg"
+  },
+  {
+    "revision": "d9db1d4246f08cc2aa943028cdc5db43",
+    "url": "/static/media/leaf.d9db1d42.svg"
+  },
+  {
+    "revision": "1ae982ae91514048d477577f7fa561fa",
+    "url": "/static/media/nbg.1ae982ae.jpg"
+  },
+  {
+    "revision": "5bc1c1eb7e74f1647018b31ce650c301",
+    "url": "/static/media/side-suc2.5bc1c1eb.jpg"
+  }
+]);
