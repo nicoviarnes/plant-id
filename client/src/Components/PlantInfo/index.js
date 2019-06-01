d1b5ff69ca14ebcaa39cd0f99c9561481808f52d
Lap Larry Nguyen <larrylapnguyen@gmail.com>
2019-05-31 21:11:58 -0700

    FEEDING AND WATER COLORS

--- client/src/Components/PlantInfo/index.js
+++ client/src/Components/PlantInfo/index.js
@@ -9,205 +9,242 @@ import wateringCan from "../../assets/images/wateringCan.png";
 import moment from "moment";
 
 class PlantInfo extends React.Component {
-  state = {
-    info: null,
-    watered: "",
-    wateringInterval: "",
-    fed: "",
-    feedingInterval: "",
-    plantId: "",
-    notes: null,
-    prevNotes: null
-  };
+	state = {
+		info: null,
+		watered: "",
+		wateringInterval: "",
+		fed: "",
+		feedingInterval: "",
+		plantId: "",
+		notes: null,
+		prevNotes: null,
+		needed: null
+	};
 
-  water = id => {
-    var date = moment().format("MMMM Do YYYY, h:mm:ss a");
-    const plantID = this.props.match.params.plant;
-    console.log(plantID, date);
-    API.waterPlant({ id: plantID, date }).then(res => {
-      this.setState({ watered: date });
-      return console.log(this.state.watered);
-    });
-  };
+	water = id => {
+		var date = moment().format("MMMM Do YYYY, h:mm:ss a");
+		const plantID = this.props.match.params.plant;
+		console.log(plantID, date);
+		API.waterPlant({ id: plantID, date }).then(res => {
+			this.setState({ watered: date });
+			return console.log(this.state.watered);
+		});
+	};
 
-  feed = id => {
-    var date = moment().format("MMMM Do YYYY, h:mm:ss a");
-    const plantID = this.props.match.params.plant;
-    console.log(plantID, date);
-    API.feedPlant({ id: plantID, date }).then(res => {
-      this.setState({ fed: date });
-      return console.log(this.state.fed);
-    });
-  };
+	feed = id => {
+		var date = moment().format("MMMM Do YYYY, h:mm:ss a");
+		const plantID = this.props.match.params.plant;
+		console.log(plantID, date);
+		API.feedPlant({ id: plantID, date }).then(res => {
+			this.setState({ fed: date });
+			return console.log(this.state.fed);
+		});
+	};
 
-  componentWillMount() {
-    const plantID = this.props.match.params.plant;
-    const { plantData } = this.props.location.state;
-    this.setState({ info: plantData });
-    API.getUserPlant(plantID).then(res => {
-      this.setState({ plantId: plantID });
-      this.setState({ info: res.data.filter(dat => dat._id === plantID) });
-      this.setState({ watered: this.state.info[0].watered });
-      this.setState({ fed: this.state.info[0].fed });
-      this.setState({ feedingInterval: this.state.info[0].feedingInterval });
-      this.setState({ wateringInterval: this.state.info[0].wateringInterval });
-    });
+	componentWillMount() {
+		const plantID = this.props.match.params.plant;
+		const { plantData } = this.props.location.state;
+		this.setState({ info: plantData });
+		API.getUserPlant(plantID).then(res => {
+			this.setState({ plantId: plantID });
+			this.setState({ info: res.data.filter(dat => dat._id === plantID) });
+			this.setState({ watered: this.state.info[0].watered });
+			this.setState({ fed: this.state.info[0].fed });
+			this.setState({ feedingInterval: this.state.info[0].feedingInterval });
+			this.setState({ wateringInterval: this.state.info[0].wateringInterval });
 
-    API.getPlantNote(plantID).then(res => {
-      this.setState({
-        notes: res.data.filter(data => data.plant === plantID)
-      });
-    });
-  }
+			var int = this.state.wateringInterval;
+			var ls = this.state.watered;
 
-  componentDidMount() {
-    const plantID = this.props.match.params.plant;
+			var needed = moment(ls).subtract(int, "days")._i;
+			var dateNoTime = needed.split(",");
+			this.setState({ needed: dateNoTime[0] });
+		});
 
-    console.log(this.state.watered);
+		API.getPlantNote(plantID).then(res => {
+			this.setState({
+				notes: res.data.filter(data => data.plant === plantID)
+			});
+		});
+	}
 
-    API.getPlantNote(plantID).then(res => {
-      this.setState({
-        notes: res.data.filter(data => data.plant === plantID)
-      });
-    });
-  }
+	componentDidMount() {
+		const plantID = this.props.match.params.plant;
 
-  removePlant(t) {
-    var self = t;
-    API.removePlant({ id: self.state.plantId }).then(res => {
-      window.location = "/manage";
-    });
-  }
+		console.log(this.state.watered);
 
-  render() {
-    const plantID = this.props.match.params.plant;
+		API.getPlantNote(plantID).then(res => {
+			this.setState({
+				notes: res.data.filter(data => data.plant === plantID)
+			});
+		});
+	}
 
-    const waterStyle = {
-      display: "inline-block",
-      width: "60px",
-      height: "60px",
-      borderRadius: "50%",
-      marginLeft: "15px",
-      marginRight: "15px"
-      // backgroundColor: "#70ef76",
-    };
-    const feederStyle = {
-      display: "inline-block",
-      width: "60px",
-      height: "60px",
-      borderRadius: "50%",
-      marginLeft: "15px",
-      marginRight: "15px"
-      // backgroundColor: "#70ef76",
-    };
+	removePlant(t) {
+		var self = t;
+		API.removePlant({ id: self.state.plantId }).then(res => {
+			window.location = "/manage";
+		});
+	}
 
-    ///////////////////////////////////////////////////
-    //////////////// LARRY PLS FIX ///////////////////
-    ///////////////////////////////////////////////////
+	render() {
+		const plantID = this.props.match.params.plant;
 
-    // get current day
-    var date = moment().format("MMMM Do YYYY");
+		const waterStyle = {
+			display: "inline-block",
+			width: "60px",
+			height: "60px",
+			borderRadius: "50%",
+			marginLeft: "15px",
+			marginRight: "15px"
+			// backgroundColor: "red"
+		};
+		const feederStyle = {
+			display: "inline-block",
+			width: "60px",
+			height: "60px",
+			borderRadius: "50%",
+			marginLeft: "15px",
+			marginRight: "15px"
+			// backgroundColor: "#70ef76",
+		};
 
-    if (this.state.watered) {
-      // last watering date and format
-      var date2 = this.state.watered.split(",");
-      var lastWatered = moment(date2[0], "MMMM Do YYYY");
+		///////////////////////////////////////////////////
+		//////////////// LARRY PLS FIX ///////////////////
+		///////////////////////////////////////////////////
 
-      lastWatered = lastWatered.format("MMMM Do YYYY");
-    }
+		// get current day
+		var date = moment().format("MM-DD-YYYY");
 
-    if (this.state.fed) {
-      var date3 = this.state.fed.split(",");
-      var lastFed = moment(date3[0], "MMMM Do YYYY");
-      lastFed = lastFed.format("MMMM Do YYYY");
-    }
+		if (this.state.watered) {
+			console.log(this.state.watered);
+			// last watering date and format
+			var date2 = this.state.watered.split(",");
+			var lastWatered = moment(date2[0], "MMMM Do YYYY");
+			lastWatered = lastWatered.format("MM-DD-YYYY");
+		}
 
-    date = moment(date, "MMMM Do YYYY")
-      .subtract(this.state.wateringInterval, "days")
-      .format("MMMM Do YYYY");
-    
-    if (moment(lastWatered).isAfter(date) === false) {
-      waterStyle.backgroundColor = "#70ef76";
-    } else {
-      waterStyle.backgroundColor = "#ebf271";
-    }
-    if (moment(lastFed).isAfter(date) === false) {
-      feederStyle.backgroundColor = "#70ef76";
-    } else {
-      feederStyle.backgroundColor = "#ebf271";
-    }
-    // in here compare the last watering date with this.state.wateringInterval
-    // do the same with feeding date and interval
-    // if the plant doesn't need to be watered -> waterStyle.backgroundColor = "#70ef76"
-    // if the plant DOES need to be watered -> waterStyle.backgroundColor = a light yellow color or something
+		var waterDate = moment(lastWatered, "MM-DD-YYYY")
+			.add(this.state.wateringInterval, "days")
+			.format("MM-DD-YYYY");
 
-    console.log(date, lastWatered);
-    ///////////////////////////////////////////////////
-    //////////////// LARRY PLS FIX ///////////////////
-    ///////////////////////////////////////////////////
+		if (this.state.watered) {
+			// console.log("Needed: ", this.state.needed);
+			// console.log("Today: ", date);
+			// console.log("Next Water: ", waterDate);
+			// console.log("Last Watered", lastWatered);
+			// console.log(
+			// 	`If "${lastWatered}" is between "${date}" and "${waterDate}"`
+			// );
+			// console.log(moment(date).isBetween(lastWatered, waterDate, null, []));
 
-    return (
-      <>
-        <div className="TOP">
-          <Grid container>
-            <Grid item xs={12} sm={12}>
-              <h1 className="info-title">
-                {this.state.info && this.state.info[0].name}
-              </h1>
-            </Grid>
-            <Grid item xs={12} sm={1} />
-            <Grid item xs={12} sm={12} md={4}>
-              <div className="card-wrapBig">
-                <div className="img-containerBig">
-                  <img
-                    className="userUploadBig"
-                    src={this.state.info[0].url}
-                    alt="user uploaded"
-                  />
-                </div>
-                <h4>
-                  Last Watering:{" "}
-                  <span class="waterSpan">
-                    {this.state.watered !== undefined
-                      ? moment(
-                          this.state.watered,
-                          "MMMM Do YYYY, h:mm:ss a"
-                        ).fromNow()
-                      : "Never!"}
-                  </span>
-                </h4>
-                <h4>
-                  Last Feeding:{" "}
-                  <span class="feedSpan">
-                    {this.state.fed !== undefined
-                      ? moment(
-                          this.state.fed,
-                          "MMMM Do YYYY, h:mm:ss a"
-                        ).fromNow()
-                      : "Never!"}
-                  </span>
-                </h4>
-                <div className="careButtons">
-                  <div className="waterBig" style={waterStyle}>
-                    <img
-                      className="wateringCanBig"
-                      src={wateringCan}
-                      onClick={() => this.water(plantID)}
-                      alt="watering Can"
-                    />
-                  </div>
-                  <div className="feederBig" style={feederStyle}>
-                    <img
-                      className="feedBig"
-                      src={
-                        "https://cdn3.iconfinder.com/data/icons/ecology-caramel-vol-1/512/FERTILIZE_THE_PLANTS-512.png"
-                      }
-                      onClick={() => this.feed(plantID)}
-                      alt="feeding Can"
-                    />
-                  </div>
-                </div>
-                {/* <Button
+			var dying = moment(waterDate)
+				.add(3, "d")
+				.format("MM-DD-YYYY");
+			console.log("Dying", dying);
+			console.log(`Dying If "${date}" is after "${dying}"`);
+
+			if (moment(date).isBetween(lastWatered, waterDate, null, [])) {
+				waterStyle.backgroundColor = "green";
+			} else if (moment(date).isAfter(dying)) {
+				waterStyle.backgroundColor = "red";
+			} else if (moment(date).isAfter(waterDate)) {
+				waterStyle.backgroundColor = "yellow";
+			}
+		}
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~End of Watering~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		if (this.state.fed) {
+			var dateSplitF = this.state.fed.split(",");
+			var lastFed = moment(dateSplitF[0], "MMMM Do YYYY");
+			lastFed = lastFed.format("MM-DD-YYYY");
+		}
+
+		var fedDate = moment(lastFed, "MM-DD-YYYY")
+			.add(this.state.feedingInterval, "days")
+			.format("MM-DD-YYYY");
+
+		if (this.state.fed) {
+			var starve = moment(fedDate)
+				.add(3, "d")
+				.format("MM-DD-YYYY");
+			console.log("Dying", starve);
+			console.log(`Starve If "${date}" is after "${starve}"`);
+
+			if (moment(date).isBetween(lastFed, fedDate, null, [])) {
+				feederStyle.backgroundColor = "green";
+			} else if (moment(date).isAfter(starve)) {
+				feederStyle.backgroundColor = "red";
+			} else if (moment(date).isAfter(fedDate)) {
+				feederStyle.backgroundColor = "yellow";
+			}
+		}
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~End of Feeding~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		///////////////////////////////////////////////////
+		//////////////// LARRY PLS FIX ///////////////////
+		///////////////////////////////////////////////////
+
+		return (
+			<>
+				<div className="TOP">
+					<Grid container>
+						<Grid item xs={12} sm={12}>
+							<h1 className="info-title">
+								{this.state.info && this.state.info[0].name}
+							</h1>
+						</Grid>
+						<Grid item xs={12} sm={1} />
+						<Grid item xs={12} sm={12} md={4}>
+							<div className="card-wrapBig">
+								<div className="img-containerBig">
+									<img
+										className="userUploadBig"
+										src={this.state.info[0].url}
+										alt="user uploaded"
+									/>
+								</div>
+								<h4>
+									Last Watering:{" "}
+									<span className="waterSpan">
+										{this.state.watered !== undefined
+											? moment(
+													this.state.watered,
+													"MMMM Do YYYY, h:mm:ss a"
+											  ).fromNow()
+											: "Never!"}
+									</span>
+								</h4>
+								<h4>
+									Last Feeding:{" "}
+									<span className="feedSpan">
+										{this.state.fed !== undefined
+											? moment(
+													this.state.fed,
+													"MMMM Do YYYY, h:mm:ss a"
+											  ).fromNow()
+											: "Never!"}
+									</span>
+								</h4>
+								<div className="careButtons">
+									<div className="waterBig" style={waterStyle}>
+										<img
+											className="wateringCanBig"
+											src={wateringCan}
+											onClick={() => this.water(plantID)}
+											alt="watering Can"
+										/>
+									</div>
+									<div className="feederBig" style={feederStyle}>
+										<img
+											className="feedBig"
+											src={
+												"https://cdn3.iconfinder.com/data/icons/ecology-caramel-vol-1/512/FERTILIZE_THE_PLANTS-512.png"
+											}
+											onClick={() => this.feed(plantID)}
+											alt="feeding Can"
+										/>
+									</div>
+								</div>
+								{/* <Button
                   variant="contained"
                   color="secondary"
                   className="delete d"
@@ -216,27 +253,27 @@ class PlantInfo extends React.Component {
                   Remove From Garden
                   <DeleteIcon />
                 </Button> */}
-              </div>
-              {/* <img
+							</div>
+							{/* <img
                 className="userUpload"
                 src={this.state.info[0].url}
                 alt="user uploaded"
               /> */}
-            </Grid>
-            <Grid item xs={12} sm={1} />
+						</Grid>
+						<Grid item xs={12} sm={1} />
 
-            <Grid item xs={12} sm={12} md={5}>
-              <CenteredTabs
-                info={this.state.info}
-                plantId={this.state.plantId}
-                notes={this.state.notes}
-              />
-            </Grid>
-          </Grid>
-        </div>
-      </>
-    );
-  }
+						<Grid item xs={12} sm={12} md={5}>
+							<CenteredTabs
+								info={this.state.info}
+								plantId={this.state.plantId}
+								notes={this.state.notes}
+							/>
+						</Grid>
+					</Grid>
+				</div>
+			</>
+		);
+	}
 }
 
 export default PlantInfo;
